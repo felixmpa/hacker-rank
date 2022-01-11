f6cd252b74fe337433856c991407f2fc0ae4194f
= <=>
2022-01-11 18:14:52 -0400

    Sock Merchant - HackerRank

--- sockMerchant.php
+++ sockMerchant.php
@@ -35,6 +35,16 @@
 * 3
 */
 
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
 function sockMerchant($n, $ar) {
     $colors = [];
     $pairs  = 0;
