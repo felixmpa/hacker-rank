0148b46b62979af35fa1491b96fb4118e4ec27bd
= <=>
2022-01-11 18:13:29 -0400

    Counting Vallyes - HackerRank

--- countingValleys.php
+++ countingValleys.php
@@ -41,6 +41,15 @@
 * 1
 */
 
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
 function countingValleys($steps, $path)
 {
     $path   = str_split($path);
