dae1bf86bc013723f85cadb14f32c17f85051d9a
= <=>
2022-02-11 10:23:04 -0400

    Counting Valleys - HackerRank

--- countingValleys.php
+++ countingValleys.php
@@ -1,6 +1,8 @@
 <?php
 /*
 *
+* Easy
+*
 * An avid hiker keeps meticulous records of their hikes. During the last hike that took exactly  steps, for every step it was noted if it was an uphill, , or a downhill,  step. Hikes always start and end at sea level, and each step up or down represents a  unit change in altitude. We define the following terms:
 *
 * A mountain is a sequence of consecutive steps above sea level, starting with a step up from sea level and ending with a step down to sea level.
@@ -66,7 +68,7 @@ function countingValleys($steps, $path){
         
         $track[$i] = $start;
 
-        if($i>0 && ($track[$i-1] == -1) && ($track[$i] == 0)  )
+        if($i>0 && ($track[$i-1] == -1) && ($track[$i] == 0))
             $valley++;
     }
 
